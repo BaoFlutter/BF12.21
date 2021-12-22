8c9e75f8f7b39af34bcc6500efc298c6e51e8200
BaoFlutter <baoflutter@gmail.com>
2021-12-22 22:14:02 +0700

    add reactive programing

--- ractive_programingprogramingg/test/ractive_programingprogramingg_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ractive_programingprogramingg/ractive_programingprogramingg.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
